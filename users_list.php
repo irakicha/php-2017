9d03158f277beaaa6dc44975c4986adb9aa31b93
root <root@std-805-2.nixsolutions.com>
2017-12-11 16:28:25 +0200

    deleted head

--- users_list.php
+++ users_list.php
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-
-Count:2
-1. Gritcenko Kirill
-5. Pakharenko Evgeny
-
-=======
-Count:3
+Count:4
 1. Gritcenko Kirill
 2. Ashykhmin Stas
 3. Pechenin Maxim
 4. Pakharenko Evgeny
->>>>>>> bff431f600b48b8bcefd18c56b0f24bb548cfa63
